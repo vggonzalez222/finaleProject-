79ebc86df898218f47bde6699a6817be24e089e0
vggonzalez222 <vg.gonzalez@ufl.edu>
2024-04-21 17:36:46 -0400

    new update

--- welcome_screen.py
+++ welcome_screen.py
@@ -46,7 +46,7 @@ def welcome_screen():
     # easy text outline
     easy_width = 110
     easy_height = 40
-    easy_x = 145
+    easy_x = 45 # change the easy_x from 145
     easy_y = HEIGHT // 2 + 30
     easy_rect_outline = pygame.Rect(easy_x, easy_y, easy_width, easy_height)
     pygame.draw.rect(screen, GREEN, easy_rect_outline)
@@ -56,13 +56,13 @@ def welcome_screen():
     easy_text = "EASY"
     easy_font = pygame.font.Font(None, DIFFICULTY_SIZE)
     easy_surf = easy_font.render(easy_text, 0, BG_COLOR)
-    easy_rect = easy_surf.get_rect(center=(200, HEIGHT // 2 + 50))
+    easy_rect = easy_surf.get_rect(center=(100, HEIGHT // 2 + 50)) # change the easy_x from 200
     screen.blit(easy_surf, easy_rect)
 
     # medium text outline
     medium_width = 110
     medium_height = 40
-    medium_x = 345
+    medium_x = 245 # change the medium_x from 345
     medium_y = HEIGHT // 2 + 30
     medium_rect_outline = pygame.Rect(medium_x, medium_y, medium_width, medium_height)
     pygame.draw.rect(screen, ORANGE, medium_rect_outline)
@@ -71,13 +71,13 @@ def welcome_screen():
     medium_text = "MEDIUM"
     medium_font = pygame.font.Font(None, DIFFICULTY_SIZE)
     medium_surf = medium_font.render(medium_text, 0, BG_COLOR)
-    medium_rect = medium_surf.get_rect(center=(400, HEIGHT // 2 + 50))
+    medium_rect = medium_surf.get_rect(center=(300, HEIGHT // 2 + 50)) # change the medium_x from 400
     screen.blit(medium_surf, medium_rect)
 
     # hard text outline
     hard_width = 110
     hard_height = 40
-    hard_x = 545
+    hard_x = 445 # change the hard_x from 545
     hard_y = HEIGHT // 2 + 30
     hard_rect_outline = pygame.Rect(hard_x, hard_y, hard_width, hard_height)
     pygame.draw.rect(screen, RED, hard_rect_outline)
@@ -86,7 +86,7 @@ def welcome_screen():
     hard_text = "HARD"
     hard_font = pygame.font.Font(None, DIFFICULTY_SIZE)
     hard_surf = hard_font.render(hard_text, 0, BG_COLOR)
-    hard_rect = hard_surf.get_rect(center=(600, HEIGHT // 2 + 50))
+    hard_rect = hard_surf.get_rect(center=(500, HEIGHT // 2 + 50)) # change the hard_x from 600
     screen.blit(hard_surf, hard_rect)
 
     # Difficulty Buttons
